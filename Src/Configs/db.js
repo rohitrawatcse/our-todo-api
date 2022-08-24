b0fd3281aa663cfd35c9fe7d22755e048e82e5c5
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2022-08-24 09:19:30 +0530

    Update db.js

--- Src/Configs/db.js
+++ Src/Configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-return mongoose.connect("mongodb+srv://sanra:adminpass@cluster0.puvpv.mongodb.net/todoapp?retryWrites=true&w=majority");
+return mongoose.connect(process.env.PORT);
 };
 
 // mongodb+srv://Alok0818:8308303270@cluster0.pmkry.mongodb.net/To-Do-Application?retryWrites=true&w=majority
